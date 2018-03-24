504c6a6920c9248784260d80493ac142c3f59188
jeojoe <jirat.onaree@gmail.com>
2018-03-24 12:33:35 +0700

    Migrate colors

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -11,4 +11,35 @@ export default {
   warningText: '#666804',
   noticeBackground: tintColor,
   noticeText: '#fff',
+
+  // ci
+  main: '#E30560',
+  mainDark: '#B90152',
+  rewardBgRight: '#F572B4',
+  rewardBgLeft: '#EF1D6B',
+
+  // bg & border
+  bgGrey: '#F9F9F9',
+  border: '#979797',
+  mainGradientTopLeft: 'rgba(252, 11, 105, 0.85)',
+  mainGradientBottomRight: 'rgba(203, 17, 80, 1)',
+
+  // input
+  lightPink: '#FF4A8D',
+  textOnLightPink: '#FFECF3',
+
+  // button
+  buttonLeft: '#FF006F',
+  buttonRight: '#C50950',
+  buttonShadowOnPink: 'rgba(127,0,51,0.5)',
+
+  // text
+  textGrey: '#525252',
+  textGreyLighter: '#777',
+
+  // label
+  rating: '#FFD12E',
+
+  // shadow
+  shadow: 'rgba(129,129,129,.5)',
 };
