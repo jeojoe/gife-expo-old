d1c892410656f7cd241607e5eb0c6da838da47cb
jeojoe <jirat.onaree@gmail.com>
2018-03-24 11:56:04 +0700

    Change entry point, remove unuse script

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { AppLoading, Asset, Font } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
 
 import RootNavigation from 'navigators/RootNavigation';
-import configureStore from './src/configureStore';
+import configureStore from './configureStore';
 
 class App extends React.Component {
   state = {
@@ -15,15 +15,15 @@ class App extends React.Component {
   _loadResourcesAsync = async () => (
     Promise.all([
       Asset.loadAsync([
-        require('./src/assets/images/robot-dev.png'),
-        require('./src/assets/images/robot-prod.png'),
+        require('./assets/images/robot-dev.png'),
+        require('./assets/images/robot-prod.png'),
       ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
         ...Ionicons.font,
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
         // to remove this if you are not using it in your app
-        'space-mono': require('./src/assets/fonts/SpaceMono-Regular.ttf'),
+        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
       }),
     ])
   );
