09db0a80c2028115dd39d053b8f6427d66853889
jeojoe <jirat.onaree@gmail.com>
2018-03-24 16:21:10 +0700

    Fix font naming

--- src/modules/Global/components/MonoText.js
+++ src/modules/Global/components/MonoText.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { Text } from 'react-native';
 
 export default props => (
-  <Text {...props} style={[props.style, { fontFamily: 'space-mono' }]} />
+  <Text {...props} style={[props.style, { fontFamily: 'thFancy-regular' }]} />
 );
