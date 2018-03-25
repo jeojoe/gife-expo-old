28e9eef33f613d50799d58312ded48704af1d6a9
jeojoe <jirat.onaree@gmail.com>
2018-03-25 11:36:09 +0700

    Add global redux action type

--- src/Global/constants/ActionTypes.js
+++ src/Global/constants/ActionTypes.js
@@ -1,4 +1,6 @@
+// Global
 export const TEST = 'TEST';
+export const SET_IS_LOADING = 'SET_IS_LOADING';
 // Auth
 export const SET_IS_LOGGEDIN = 'SET_IS_LOGGEDIN';
 export const SET_INVITATION_CODE = 'SET_INVITATION_CODE';
