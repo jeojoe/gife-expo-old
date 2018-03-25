e1fc231cb086eaf3e3860a1d2e1b4959acf13975
jeojoe <jirat.onaree@gmail.com>
2018-03-25 12:10:13 +0700

    Add config

--- src/Global/constants/index.js
+++ src/Global/constants/index.js
@@ -2,10 +2,12 @@ import Colors from './Colors';
 import Layout from './Layout';
 import * as ActionTypes from './ActionTypes';
 import * as GlobalPropTypes from './GlobalPropTypes';
+import Config from './Config';
 
 export {
   Colors,
   Layout,
   ActionTypes,
   GlobalPropTypes,
+  Config,
 };
